f77178a1a1d63cf44404cdfe6da17d20867f07be
Pranav Chaudhary <pranavc2010@gmail.com>
2024-01-17 14:14:59 -0800

    4th commit, fingers crossed last one for tonight

--- src/v1/app.py
+++ src/v1/app.py
@@ -9,9 +9,11 @@ CORS(app)
 for blueprint in blueprints:
     app.register_blueprint(blueprint)
 
+#arman to fix up using his sessions knowledge. but db created here and imported in __init__.py of the subfolder whose function needs to access it
 db=SQLAlchemy()
 
 #use different URIs for different environments so we can add/remove columns/tables without breaking production code
+#see pramods structure of having a config folder with test, dev and prod db
 app.config['SQLALCHEMY_DATABASE_URI']=''
 
 db.init_app(app)
--- src/v1/utils/get_user_id_from_access_token.py
+++ src/v1/utils/get_user_id_from_access_token.py
@@ -1,4 +1,4 @@
-# Desc: Returns user_id from access_token using SQL query
+# Desc: Returns user_id from access_token using SQL query on the db imported in __inti__.py
 def get_user_id_from_access_token(access_token):
     try:
         user_id=tokens_model.query.filter_by(token=access_token).first().user_id
